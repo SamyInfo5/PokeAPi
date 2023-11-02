ab617e1f8b5b8fa48a1051fb9ef8b105da2c9418
SamyInfo5 <samueldhondt@MacBook-Air-de-Samuel.local>
2023-11-02 14:41:23 +0100

    interface

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,39 @@
 <template>
     <header>
-        <h1>PokeApi NuxtJs</h1>
+        <div class="container">
+            <h1>PokeApi NuxtJs</h1>
+        </div>
     </header>
-    
+    <main>
+        <div>
+            <div class="container">
+                <div v-if="pokemon" class="pokemonBox">
+                    <div class="pokemonImage">
+                        <img :src="pokemon.sprites.front_default" alt="Pokemon Image" />
+                    </div>
+                    <div class="pokemon">
+                        <div class="listPokemon">
+                            <h1>{{ pokemon.name }}</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="detailsPokemon">
+            <div class="container">
+                <div class="statOfPokemon">
+                    <h1>coucou</h1>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <div class="container">
+            <div class="containeFooter">
+            </div>
+        </div>
+    </footer>
 </template>
+
+<script setup>
+</script>
