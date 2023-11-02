e439ba0b5b652825de4abfdb907302a754eafe15
SamyInfo5 <samueldhondt@MacBook-Air-de-Samuel.local>
2023-11-02 16:14:46 +0100

    add a liste of pokemon

---
+++ components/listPokemon.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>Liste des Pokémon</h1>
+        <ul>
+            <li class="pokemonname" v-for="pokemon in pokemonList" :key="pokemon.id">
+                {{ pokemon.name }}
+            </li>
+        </ul>
+    </div>
+</template>
+  
+<script setup>
+const pokemonList = ref([]);
+
+const fetchPokemonList = async (offset = 0) => {
+    const { data } = await useFetch(`https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=1290/`);
+    pokemonList.value = data._rawValue.results;
+    console.log(data)
+};
+
+// Appeler la fonction pour récupérer la liste initiale de Pokémon
+fetchPokemonList();
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,44 @@
 <template>
-    <header>
-        <div class="container">
-            <h1>PokeApi NuxtJs</h1>
-        </div>
-    </header>
-    <main>
-        <div>
-            <div class="container">
-                <div class="pokemonBox">
-                    <div class="pokemonImage">
-                        <img :src="data.sprites.front_default" alt="">
-                    </div>
-                    <div class="pokemon">
-                        <div class="listPokemon">
-                           <h1>{{ data.name }}</h1>
-                        </div>
-                    </div>
-                </div>
+  <header>
+    <div class="container">
+      <h1>PokeApi NuxtJs</h1>
+    </div>
+  </header>
+  <main>
+    <div>
+      <div class="container">
+        <div class="pokemonBox">
+          <div class="pokemonImage">
+            <img :src="data.sprites.front_default" alt="" />
+          </div>
+          <div class="pokemon">
+            <div class="listPokemon">
+              <listPokemon/>
             </div>
+          </div>
         </div>
-        <div class="detailsPokemon">
-            <div class="container">
-                <div class="statOfPokemon">
-                    <div v-for="(item, i) in data.stats" key="i">{{ item.stat.name }} : {{ item.base_stat }}</div>
-                </div>
-            </div>
+      </div>
+    </div>
+    <div class="detailsPokemon">
+      <div class="container">
+        <div class="statOfPokemon">
+          <div class="stat" v-for="(item, i) in data.stats" key="i">
+            {{ item.stat.name }} : {{ item.base_stat }}
+          </div>
         </div>
-    </main>
-    <footer>
-        <div class="container">
-            <div class="containeFooter">
-                
-            </div>
-        </div>
-    </footer>
+      </div>
+    </div>
+  </main>
+  <footer>
+    <div class="container">
+      <div class="containeFooter"></div>
+    </div>
+  </footer>
 </template>
 
 <script setup>
-
-import '~/assets/styles.css'
-const id = ref(1)
-const { data } = await useFetch(`https://pokeapi.co/api/v2/pokemon/${id.value}`)
+import "~/assets/styles.css";
+const id = ref(1);
+const { data } = await useFetch(`https://pokeapi.co/api/v2/pokemon/${id.value}`);
 
 </script>
