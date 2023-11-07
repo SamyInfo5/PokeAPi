538acab6519f5a79656f4f5f5f9a9540fc0fa558
SamyInfo5 <samueldhondt@MacBook-Air-de-Samuel.local>
2023-11-07 14:05:29 +0100

    create indexpage

--- pages/pokemon/[name].vue
+++ pages/pokemon/[name].vue
@@ -1,42 +1,49 @@
 <template>
-    <header class="headerPokemon">
-        <h1>Voici : {{ $route.params.name }} <img :src="pokemon.sprites.front_default" alt=""></h1>
-    </header>
-    <main>
-        <div>
-            <div class="container">
-            <div class="classPokemon">
-                <h1>Pokemon de Types :</h1>
-                <div class="types" v-for="(items, j) in pokemon.types" key="j">
-                    <p>{{ items.type.name }}</p>
-                </div>
-            </div>
-
-                <h1>Abiliter du Pokemon :</h1>
-                <div class="types" v-for="(items, j) in pokemon.abilities" key="j">
-                    <p>{{ items.ability.name }}</p>
-                </div>
-
-                <h1>Expérience de Base :</h1>
-                <div class="exp">
-                    {{ pokemon.base_experience }}
-                </div>
-
-                <h1>Toutes les attaques que le pokemon peux apprendre</h1>
-                <div class="types" v-for="(items, j) in pokemon.moves" key="j">
-                    <h3 class="attack">{{ items.move.name }}</h3>
-                </div>
-            </div>
+  <header class="headerPokemon">
+  <button> <a href="/acceuil/index.vue">Back To acceuil</a> </button>
+    <h1>Voici : {{ $route.params.name }}</h1>
+    <img :src="pokemon.sprites.front_default" alt="" />
+  </header>
+  <main>
+    <div>
+      <div class="container">
+        <div class="classPokemon">
+          <h1>Pokemon de Types :</h1>
+          <div class="types" v-for="(items, j) in pokemon.types" key="j">
+            <p>{{ items.type.name }}</p>
+          </div>
         </div>
-    </main>
-    <footer>
-        <h1>designed by Samuel</h1>
-    </footer>
+        <div class="abilitys">
+          <h1>Abiliter du Pokemon :</h1>
+          <div class="abi" v-for="(items, j) in pokemon.abilities" key="j">
+            <p>{{ items.ability.name }}</p>
+          </div>
+        </div>
+        <div class="expBase">
+          <h1>Expérience de Base :</h1>
+          <div class="exp">
+            {{ pokemon.base_experience }}
+          </div>
+        </div>
+        <div class="allAttack">
+          <h1>Toutes les attaques que le pokemon peux apprendre</h1>
+          <div class="atck">
+            <button class="attack" v-for="(items, j) in pokemon.moves" key="j">{{ items.move.name }}</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+  <footer>
+    <h1>designed by Samuel</h1>
+  </footer>
 </template>
 
 <script setup>
-import "~/assets/singlepokmn.css";
-const route = useRoute()
-const { data: pokemon } = await useFetch(`https://pokeapi.co/api/v2/pokemon/${route.params.name}`)
-console.log(pokemon)
+import "~/assets/singlepkmn.css";
+const route = useRoute();
+const { data: pokemon } = await useFetch(
+  `https://pokeapi.co/api/v2/pokemon/${route.params.name}`
+);
+console.log(pokemon);
 </script>
