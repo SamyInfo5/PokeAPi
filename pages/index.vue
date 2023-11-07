cacd7ac9a4a34f0d6dfe1b9fc276123ace878a1c
SamyInfo5 <samueldhondt@MacBook-Air-de-Samuel.local>
2023-11-07 11:57:42 +0100

    add: Page for sigle pokemon with info

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
           </div>
           <div class="pokemon">
             <div class="listPokemon">
-              <listPokemon />
+              <listPokemon :pokemonList="pokemonList" />
             </div>
           </div>
         </div>
@@ -23,21 +23,49 @@
       <div class="container">
         <div class="statOfPokemon">
           <div class="stat" v-for="(item, i) in data.stats" key="i">
-            {{ item.stat.name }} : {{ item.base_stat }}
+            <h6>{{ item.stat.name }} : {{ item.base_stat }}</h6>
+          </div>
+        </div>
+        <div class="caract">
+          <div>
+            <h1>Types</h1>
+            <div class="types" v-for="(items, j) in data.types" key="j">
+              <p>{{ items.type.name }}</p>
+            </div>
+          </div>
+          <div>
+            <h1>localisation</h1>
+            <div class="localisation" v-for="(item, k) in localisation" key="k">
+              <pre>{{ item.location_area.name }}</pre>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </main>
-  <footer>
-    <div class="container">
-      <div class="containeFooter"></div>
-    </div>
-  </footer>
 </template>
 
 <script setup>
 import "~/assets/styles.css";
+
 const id = ref(1);
-const { data } = await useFetch(`https://pokeapi.co/api/v2/pokemon/${id.value}`);
+const pokemonList = ref([])
+
+const { data } = await useFetch(`
+  https://pokeapi.co/api/v2/pokemon/${id.value}
+`);
+
+const { data: localisation } = await useFetch(
+  `https://pokeapi.co/api/v2/pokemon/${id.value}/encounters`
+);
+
+const fetchPokemonList = async (offset = 0) => {
+  const { data } = await useFetch(`https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=1290/`);
+  pokemonList.value = data._rawValue.results;
+  console.log('pokemonList', pokemonList.value)
+};
+
+
+
+fetchPokemonList();
 </script>
